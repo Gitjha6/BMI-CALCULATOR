2af3f3f67d6c995297c3bbe6a4057c4deac31dc5
Gitjha6 <136634839+Gitjha6@users.noreply.github.com>
2023-09-24 15:16:11 +0530

    Add files via upload

---
+++ BMI.js
@@ -0,0 +1,20 @@
+function cal()
+{
+ var w=document.getElementById('weight').value;
+ var h=document.getElementById('height').value;
+ var result=(w/(h*h)).toFixed(1);
+ document.getElementById('res').value=result; 
+
+if(result<18.5){
+    document.getElementById('range').innerHTML="Underweight";
+}
+else if(18.5<=result && result<25){
+    document.getElementById('range').innerHTML="Healthy";
+}
+else if(25<=result && result<30){
+    document.getElementById('range').innerHTML="Overweight";
+}
+else  {
+    document.getElementById('range').innerHTML="obesity";
+}
+}
